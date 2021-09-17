d38ff620be69a23e87f58e7dda8c312118b1cf95
vittorioc <vittorio.caiafa@gmail.com>
2021-09-17 15:49:39 -0300

    se borraron archivos giuli

--- src/consolalinux/User_.java
+++ src/consolalinux/User_.java
@@ -10,12 +10,17 @@ public class User_ {
     
     private String name;
     private String password;
-           
+    private ArrayList<Folder_> folder;
+    
     public String getName(){
         return this.name;
     } 
     public String getPassword(){
         return this.password;
+    }
+    
+    public ArrayList<Folder_> getFiles() { // cambiar
+        return folder;
     }
     
     public void setName(String name) {
@@ -24,6 +29,10 @@ public class User_ {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    
+    public void setFiles(ArrayList<Folder_> files) { // cambiar
+        this.folder = files;
     }
     
 }
