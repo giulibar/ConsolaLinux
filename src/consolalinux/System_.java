afe4731137f03ad5959b9f47ce424d6ed12f8589
Giuliano Bardecio <giulibardecio@gmail.com>
2021-09-17 15:57:33 -0300

    agrego getUser

--- src/consolalinux/System_.java
+++ src/consolalinux/System_.java
@@ -1,43 +1,53 @@
 /*
 Nombres: Giuliano Bardecio [256113] y Vittorio Caiafa [252295]
-*/
-
+ */
 package consolalinux;
 
 import java.util.*;
 
 public class System_ {
+
     private ArrayList<User_> userList;
     private ArrayList<Folder_> foldersList;
     private User_ loggedUser;
-    
+
     public System_() {
         userList = new ArrayList<>();
         foldersList = new ArrayList<>();
         loggedUser = new User_();
     }
-    
-    public ArrayList<User_> getUserList(){
+
+    public ArrayList<User_> getUserList() {
         return userList;
     }
-    
-    public ArrayList<Folder_> getFolderList(){
+
+    public ArrayList<Folder_> getFolderList() {
         return foldersList;
     }
-    
-    public User_ getLoggedUser(){
+
+    public User_ getUser(String name) {
+        User_ user = null;
+        for (User_ userInList : userList) {
+            if (userInList.getName().equals(name)) {
+                user = userInList;
+            }
+        }
+        return user;
+    }
+
+    public User_ getLoggedUser() {
         return this.loggedUser;
     }
-    
-    public void addUser(User_ user){
+
+    public void addUser(User_ user) {
         userList.add(user);
     }
-    
-    public void addFolder(Folder_ folder){
+
+    public void addFolder(Folder_ folder) {
         foldersList.add(folder);
     }
-    
-    public void setLoggedUser(User_ user){
+
+    public void setLoggedUser(User_ user) {
         loggedUser = user;
     }
 }
